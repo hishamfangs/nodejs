223b7f6c042d358e91abd62ec8ba2f07aadd7846
hishamfangs <hishamfangs@gmail.com>
2024-06-07 15:07:37 +0400

    Finished Authemtication & Register & Login Mechanism

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const { resolve } = require("path");
 const mysql = require("mysql2/promise");
-const fs = require("node:fs");
+const dotenv = require("dotenv");
+dotenv.config();
 const cors = require("cors");
 const multer = require("multer");
 const UPLOAD_FILES_DIR = "./uploads";
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
 const app = express();
 const port = process.env.PORT || 3010;
 var con;
 
 // Setup cors
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Create SQL Connection
 createSQLConnection();
@@ -20,7 +26,7 @@ const storage = multer.diskStorage({
 	destination(req, file, cb) {
 		cb(null, UPLOAD_FILES_DIR);
 	},
-	// in case you want to change the names of your files)
+	// Change the name of the file for security
 	filename(req, file = {}, cb) {
 		//file.mimetype = "audio/webm";
 		// console.log(req)
@@ -32,12 +38,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 async function createSQLConnection() {
-	con = await mysql.createConnection(
-		`mysql://avnadmin:AVNS_a66tKd6_5pdZrtTP9ib@mysql-hisham-astratech-interview-hishamfangs-8c23.f.aivencloud.com:21650/defaultdb?ssl-mode=REQUIRED`
-	);
-
+	con = await mysql.createConnection(process.env.DBCONNECTION_STRING);
 	await connectToSQL();
-	//await selectStatement();
 }
 
 async function connectToSQL() {
@@ -52,11 +54,6 @@ async function connectToSQL() {
 		});
 	});
 }
-/* app.use(express.static("static"));
-
-app.get("/", (req, res) => {
-	res.sendFile(resolve(__dirname, "pages/index.html"));
-}); */
 
 /*****
  *
@@ -147,6 +144,11 @@ app.post("/uploadFiles", upload.array("files", 5), async function (req, res) {
 	}
 });
 
+/*****
+ *
+ *  Update Files: simply updates the datetime_updated value
+ *
+ *********************/
 app.put("/update/:id", async (req, res) => {
 	try {
 		const [results, fields] = await con.query(
@@ -166,6 +168,11 @@ app.put("/update/:id", async (req, res) => {
 	res.end;
 });
 
+/*****
+ *
+ *  Deletes File from Database
+ *
+ *********************/
 app.delete("/delete/:id", async (req, res) => {
 	try {
 		const [results, fields] = await con.query(
@@ -182,6 +189,102 @@ app.delete("/delete/:id", async (req, res) => {
 		res.send({ success: false, message: err });
 	}
 	res.end();
+});
+
+/************************/
+/**** Authentication ****/
+/************************/
+
+const generateAccessToken = (userId) => {
+	return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
+const getUser = async (username) => {
+	const query = `SELECT * FROM tb_users WHERE username = '${username}'`;
+	var [results, fields] = await con.query(query);
+	return results;
+};
+
+/*****
+ *  Register New User: disabled for now
+ *****************************************/
+/* 
+const insertUser = async (username, password) => {
+	const query = `INSERT INTO tb_users SET username = '${username}', password = '${password}'`;
+	var [results, fields] = await con.query(query);
+	return results;
+};
+
+app.get("/register", async (req, res) => {
+	// const { email, password } = req.body;
+	const { username, password } = req.query;
+	if (!username || !password) {
+		res
+			.status(400)
+			.json({ error: "username or Password fields cannot be empty!" });
+		return;
+	}
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(password, salt);
+	const user = {
+		username,
+		password: hashedPassword,
+	};
+	try {
+		//const userAlreadyExists = await checkRecordExists("users", "username", username);
+		//if (userAlreadyExists) {
+		//  res.status(409).json({ error: "username already exists" });
+		//} else {
+		const results = await insertUser(username, hashedPassword);
+		res.status(201).json({ message: "User created successfully!" });
+		//}
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+});
+ */
+
+/*****
+ *
+ *  Login User
+ *
+ *****************************************/
+app.post("/login", async (req, res) => {
+	const { username, password } = req.body;
+	if (!username || !password) {
+		res
+			.status(400)
+			.json({ error: "Username or Password fields cannot be empty!" });
+		return;
+	}
+
+	try {
+		const existingUser = await getUser(username);
+
+		if (existingUser && existingUser.length) {
+			if (!existingUser[0].password) {
+				res.status(401).json({ error: "Invalid credentials" });
+				return;
+			}
+
+			const passwordMatch = await bcrypt.compare(
+				password,
+				existingUser[0].password
+			);
+
+			if (passwordMatch) {
+				res.status(200).json({
+					username: existingUser[0].username,
+					access_token: generateAccessToken(existingUser[0].username),
+				});
+			} else {
+				res.status(401).json({ error: "Invalid credentials" });
+			}
+		} else {
+			res.status(401).json({ error: "Invalid credentials" });
+		}
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
 });
 
 app.listen(port, () => {
